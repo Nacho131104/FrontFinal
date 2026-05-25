cae9e76b3fa6b3f3490c1f555bafeda706511dff
Nacho <nacholopezamat@gmail.com>
2026-05-25 05:53:28 -0400

    pagina principal y characters

---
+++ src/app/components/character/character.tsx
@@ -0,0 +1,28 @@
+"use client"
+import type {Character} from "@/types/types"
+import "./character.css"
+import {useRouter} from "next/navigation"
+
+const CharacterSerie = ( {personaje}:{personaje: Character} ) =>{
+
+
+    const router= useRouter()
+    return (
+        <div className="CharacterInfo">
+            <h1  onClick={()=>{router.push(`/character/${personaje.id}`)}} className="text">{personaje.name}</h1>
+            <div className="characterInfoSub">
+
+                <img src={personaje.image}></img>
+                <div className="CharacterInfoText">
+                    <p> Status: {personaje?.status}</p>
+                    <p> Species: {personaje?.species}</p>
+                    <p> Gender: {personaje?.gender}</p>
+                    <img src={personaje?.image} />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default CharacterSerie;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import "./page.css";
+import {useRouter} from "next/navigation"
+import {useState, useEffect} from "react"
+import type {Character, Response } from "../types/types"
+import api from "@/api/api"
+import CharacterSerie from "./components/character/character"
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+const Home = () =>{
+    const [charactersData, setCharactersData] = useState<Response|null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
+    const [page, setPage]= useState(1)
+
+    const router = useRouter()
+
+    const [id, setId]=useState<number|null>(null)
+
+    const fetchCharacters = async ()=>{
+         await api.get(`/character?page=${page}`).then((e)=>{
+            const {data}:{data:Response} = e;
+            setCharactersData(data)
+        }).finally(()=>{
+            setLoading(false)
+        })
+    }
+
+
+    useEffect(()=>{
+        fetchCharacters()
+    },[page])
+
+    return (
+        <div className = "charactersContainer">
+            <h1> Welcome to characters</h1>
+            {loading && (<p>Loading ....</p>)}
+            {charactersData && charactersData.results.map((r)=>{
+                return (
+                    <CharacterSerie key= {r.id} personaje={r}></CharacterSerie>
+                )
+            })}
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
+
+
+export default Home;
